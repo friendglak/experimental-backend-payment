f2d357bd967368585fee8beef577c2aad7672740
Juan Camilo Velásquez Amarillo <camilojheans@gmail.com>
2024-02-05 14:34:16 -0500

    refac: update spec_helper.rb to configure test environment and clean database

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,48 +1,54 @@
 require 'sequel'
 require 'webmock/rspec'
 require 'factory_bot'
-require 'bundler/setup'
 require 'dotenv'
 require 'database_cleaner/sequel'
+require 'bundler/setup'
 
+# Load environment variables
 Dotenv.load
 
+# Setup Bundler for test environment
 Bundler.require(:default, :test)
 
+# Connect to test database
 TEST_DB = Sequel.connect(ENV['TEST_DATABASE_URL'])
+
+# Require models
 require_relative '../app/models/driver'
 require_relative '../app/models/ride'
 require_relative '../app/models/rider'
 
+# Disable external requests, allow localhost
 WebMock.disable_net_connect!(allow_localhost: true)
 
-DatabaseCleaner[:sequel].db = Sequel.connect("postgres://postgres:friendglak@localhost/ride_hailing_service_test")
+# Configure DatabaseCleaner to use Sequel and set the database
+DatabaseCleaner[:sequel].db = TEST_DB
 
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
+
+  # Load all factory_bot definitions
   FactoryBot.find_definitions
 
   config.before(:suite) do
+    # Clean the database before suite runs
     DatabaseCleaner[:sequel].clean_with(:truncation)
   end
 
-  # Establece la estrategia de limpieza como transacción por defecto
   config.before(:each) do
+    # Use transaction strategy and start before each test
     DatabaseCleaner[:sequel].strategy = :transaction
-  end
-
-  # Inicia la limpieza antes de cada prueba
-  config.before(:each) do
     DatabaseCleaner[:sequel].start
   end
 
-  # Limpia la base de datos después de cada prueba
   config.after(:each) do
+    # Clean up after each test
     DatabaseCleaner[:sequel].clean
   end
 end
 
-# Si tienes un bloque de configuración para tus fábricas, ajusta el método de guardado
+# Adjust FactoryBot to use Sequel's `save` method
 FactoryBot.define do
-  to_create {|instance| instance.save } # Usa `save` de Sequel en lugar de `save!`
+  to_create(&:save) # Simplified with Symbol to Proc
 end
